58f9894d9a59bd487b8ef537e89b295229b6c782
ailuria-ux <ajayvardhanreddy1310@gmail.com>
2025-08-05 12:24:44 -0400

    Merge conflicts (#7)
    * added merge-conflict.ja
    
    * Update merge-conflict.js
    
    * Update merge-conflict.js
    
    * fixed add and semi colon, conflicts
    
    * reaname sub to subtract
    
    * export add and minus
    
    * Update merge-conflict.js
    
    * sed merged conflicts edd mul function
    
    * Update a.txt
    
    ---------
    
    Co-authored-by: Ajay Vardhan Reddy <ailuriajayvardhanreddy@gmail.com>
    Co-authored-by: Ajay Vardhan Reddy Ailuri <ajayvardhanreddy1309@gmail.com>

---
+++ merge-conflict.js
@@ -0,0 +1,12 @@
+// this function adds 2 nums
+const add = (n1,n2) => n1 + n2;
+const subtract = (n1,n2) => n1 - n2;
+const div = n1 / n2;
+=======
+const mul = (n1,n2) => {
+	return n1*n2;
+}
+export {
+	add,
+	minus
+}
